9e5139b0d2412b7d659b741c874fad10b985d899
KernelA <17554646+KernelA@users.noreply.github.com>
2022-09-18 12:44:57 +0300

    Add mode choosing

--- main.py
+++ main.py
@@ -32,6 +32,16 @@ def main(args):
 
     face_indices = np.array([0, 1, 2], dtype=IndexType)
 
+    num_channels = 3 if args.image_mode == "rgb" else 4
+    rendered_image = np.zeros((args.height, args.width, num_channels), dtype=np.uint8)
+
+    mode = "RGB"
+    texture_type = GL_RGB
+
+    if num_channels == 4:
+        mode = "RGBA"
+        texture_type = GL_RGBA
+
     try:
         glClearColor(0, 0, 0.0, 1.0)
         glEnable(GL_MULTISAMPLE)
@@ -45,8 +55,6 @@ def main(args):
         glUseProgram(shader)
         # Orto projection in range [0; 1]
         set_shader_params(shader)
-
-        rendered_image = np.zeros((args.height, args.width, 4), dtype=np.uint8)
 
         render_frame_object, render_frame_ms_object = init_frame_buffer(rendered_image)
 
@@ -69,12 +77,12 @@ def main(args):
         glBindFramebuffer(GL_FRAMEBUFFER, render_frame_object)
         glReadBuffer(GL_COLOR_ATTACHMENT0)
 
-        data = glReadPixels(0, 0, args.width, args.height, GL_RGBA, GL_UNSIGNED_BYTE)
+        data = glReadPixels(0, 0, args.width, args.height, texture_type, GL_UNSIGNED_BYTE)
 
         glBindFramebuffer(GL_FRAMEBUFFER, 0)
         glUseProgram(shader)
 
-        image = Image.frombuffer("RGBA", (args.width, args.height), data)
+        image = Image.frombuffer(mode, (args.width, args.height), data)
         image.save(args.out_image, optimize=True)
     finally:
         egl.eglDestroySurface(display, egl_surf)
@@ -87,6 +95,7 @@ if __name__ == "__main__":
     parser.add_argument("--height", dest="height", type=int, default=256)
     parser.add_argument("-o", "--out_image", dest="out_image", type=str, default="out.png",
                         help="A path to the output image with render")
+    parser.add_argument("--image_mode", choices=["rgb", "rgba"], default="rgb")
 
     args = parser.parse_args()
     main(args)
